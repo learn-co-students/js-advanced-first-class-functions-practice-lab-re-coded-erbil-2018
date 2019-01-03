81ce30fd7797971d7b5672649539e2c8e9205084
zaid85j <zaid85j@gmail.com>
2019-01-03 10:40:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const logDriverNames=function(drivers){
+drivers.forEach(function (driver) {
+ console.log(driver.name)
+})
+}
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
+     })
+      }
+      const driversByRevenue = function (drivers, Revenue) {
+  drivers.forEach(function (driver) {
+    return driver.sort(driver.Revenue) 
+      console.log(driver.name);
+    
+  });
+};
